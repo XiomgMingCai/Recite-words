a27ce6304b9356998cddc470ba93c17894ef27e9
xiongmingcai <1026911109@qq.com>
2017-02-13 16:10:46 +0800

    第一个组件

---
+++ src/components/buttomBtn.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="buttomBtn">
+    <button id="btn" :class="colorName" class="text-size-lg">{{ value }}</button>
+
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  export  default{
+    name: 'bottomBtn',
+    props: ['value', 'color', 'position'],
+    computed: {
+      colorName: function () {
+        if (this.color == 'orange') {
+          return this.colorName = 'orange'
+        } else if (this.color == 'green') {
+          return this.colorName = 'green'
+        }
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="less" scoped rel="stylesheet/less">
+  @import "../assets/css/variable.less";
+
+  #buttomBtn {
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    padding: 10px 0 10px 0;
+    background-color: white;
+    /*opacity: 0.9;*/
+    background-image: url("../assets/images/opacity.png");
+    /*background-repeat:no-repeat;*/
+
+  }
+
+  #btn {
+    height: 45px;
+    width: 80%;
+    color: @white;
+    border: 0;
+   // background: linear-gradient(45deg, @green 0%, @Green 100%);
+    letter-spacing: 2px;
+  }
+
+  .orange {
+    background-color: @orange;
+  }
+
+  .green {
+    background-color: @green;
+  }
+</style>
---
+++ src/components/header-lg.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="header-lg">
+    <h3>{{bigTite}}</h3>
+    <h4>{{noteTitle}}</h4>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+    name: 'header-lg',
+    props: ['bigTite', 'noteTitle']
+  }
+</script>
+
+<style lang="less" scoped rel="stylesheet/less">
+  @import '../assets/css/variable.less';
+
+  #header-lg {
+    height: 100px;
+    box-shadow: #ddd 0 3px;
+    background: linear-gradient(45deg, @green 0%, @Green 100%);
+    padding: 50px 0 0 20px;
+  }
+
+  h3 {
+    font-size: @font-size-lg;
+    color: @white;
+  }
+
+  h4 {
+    font-size: 120%;
+    position: relative;
+    margin-top: -10px;
+  }
+</style>
