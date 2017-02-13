4318a1940cc569e7635100a1a34cfe91169bf0f4
xiongmingcai <1026911109@qq.com>
2017-02-13 12:03:18 +0800

    开始第一个页面

---
+++ store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex);
+// 状态
+var state = {
+ newbi:0//
+ };
+/**
+ * 用原生 ajax
+ */
+/*var stare, xmlhttp;
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    state = eval('(' + xmlhttp.responseText + ')')
+  }else {
+    state = {serverIP:0}
+  }
+};
+
+/!**
+ * http://127.0.0.1/
+ * @false :同步获取 默认异步
+ *!/
+xmlhttp.open("http://127.0.0.1/json/users.php",false);*/
+
+export default new Vuex.Store({
+  state
+})
